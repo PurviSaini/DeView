b8441da5dce0f349765a9ff493b5300d1ffecd41
Purvi Saini <purvitashu450@gmail.com>
2025-04-06 19:29:40 +0530

    edit vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: mode === 'production' ? 'https://de-view-backend.vercel.app' : 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      },
+      '/api': 'https://de-view-backend.vercel.app',
     },
   },
 }));
