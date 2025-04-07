f7ceecdd6fcd3574ad272ff76fe68fd2747f5e83
Shikha Dhiman <dshikha4403@gmail.com>
2025-04-07 18:46:14 +0530

    made all input field as required

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -9,12 +9,12 @@ export default function Login(){
                 {/* input for email */}
                 <label for="email"><i class="fa fa-envelope-o"></i> &nbsp;Email Address</label>
                 <br/>
-                <input type="email" name="email" id="e-mail" placeholder="example@gmail.com"/>
+                <input type="email" name="email" id="e-mail" placeholder="example@gmail.com" required/>
                 <br/>
                 {/* input for password */}
                 <label for="password"><i class="fa fa-lock"></i> &nbsp; Password</label>
                 <br/>
-                <input type="password" name="password" id="passwd" placeholder="Shh!!, it's secret"/>
+                <input type="password" name="password" id="passwd" placeholder="Shh!!, it's secret" required/>
                 <br/>
 
                 <button>Log In</button>
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -10,17 +10,17 @@ export default function Register(){
                 {/* input for username */}
                 <label for="username"><i class="fa fa-user-o"></i> &nbsp; Username</label>
                 <br />
-                <input type="text" name="username" id="username" placeholder='Enter Username'/>
+                <input type="text" name="username" id="username" placeholder='Enter Username' required/>
                 <br />
                 {/* input for email */}
                 <label for="email"><i class="fa fa-envelope-o"></i> &nbsp;Email Address</label>
                 <br/>
-                <input type="email" name="email" id="e-mail" placeholder="example@gmail.com"/>
+                <input type="email" name="email" id="e-mail" placeholder="example@gmail.com" required/>
                 <br/>
                 {/* input for password */}
                 <label for="password"><i class="fa fa-lock"></i> &nbsp; Password</label>
                 <br/>
-                <input type="password" name="password" id="passwd" placeholder="Shh!!, it's secret"/>
+                <input type="password" name="password" id="passwd" placeholder="Shh!!, it's secret" required/>
                 <br/>
                 <button>Register</button>
                 <p>Already registered? <a href="/">Login</a></p>
