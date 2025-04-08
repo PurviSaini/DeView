eb8ff2e7592df9a92f40766bf8c044668d125890
Purvi Saini <purvitashu450@gmail.com>
2025-04-08 19:52:57 +0530

    registration page connected to backend

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,36 +1,62 @@
 // import './Register.css'
-import { useNavigate, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './Login.css'
 import Navbar from './Navbar'
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register(){
     const navigate = useNavigate();
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async (e)=>{
+        e.preventDefault();
+        
+        try{
+            const res = await axios.post(import.meta.env.VITE_BACKEND_URL+ "/signup", {
+            username: username,
+            email: email,
+            password: password
+        })
+        alert(res.data.message);
+            navigate("/");
+        
+       }
+        catch(err){
+            alert("Can't Register the user")
+            console.log(err);
+        }
+    }
     return (
         <div>
             <Navbar/>
-            <form action="/">
+            <form>
                 <p id='heading'>Register</p>
                 {/* input for username */}
-                <label for="username"><i class="fa fa-user-o"></i> &nbsp; Username</label>
+                <label htmlFor="username"><i className="fa fa-user-o"></i> &nbsp; Username</label>
                 <br />
-                <input type="text" name="username" id="username" placeholder='Enter Username' required/>
+                <input type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}} name="username" id="username" placeholder='Enter Username' required/>
                 <br />
                 {/* input for email */}
-                <label for="email"><i class="fa fa-envelope-o"></i> &nbsp;Email Address</label>
+                <label htmlFor="email"><i className="fa fa-envelope-o"></i> &nbsp;Email Address</label>
                 <br/>
-                <input type="email" name="email" id="e-mail" placeholder="example@gmail.com" required/>
+                <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} name="email" id="e-mail" placeholder="example@gmail.com" required/>
                 <br/>
                 {/* input for password */}
-                <label for="password"><i class="fa fa-lock"></i> &nbsp; Password</label>
+                <label htmlFor="password"><i className="fa fa-lock"></i> &nbsp; Password</label>
                 <br/>
-                <input type="password" name="password" id="passwd" placeholder="Shh!!, it's secret" required/>
+                <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} name="password" id="passwd" placeholder="Shh!!, it's secret" required/>
                 <br/>
-                <button >Register</button>
-                <p>Already registered? <a href="/">
+                <button onClick={handleRegister}>Register</button>
+                <p>Already registered? 
                     <NavLink to="/" className="login-btn">
                     Login
                     </NavLink>
-                </a></p>
+                </p>
             </form>
         </div>
     )
