e1e44e4e28a9b6575d3a05f390cd9847afe1e638
Purvi Saini <purvitashu450@gmail.com>
2025-04-09 11:57:29 +0530

    cleaning App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
@@ -7,20 +5,8 @@ import Team from './pages/Team'
 import Sidebar from './components/Sidebar'
 import Task from './pages/Task'
 import './App.css'
-import axios from 'axios'
 
 function App() {
-  const [fruits, setFruits] = useState([]);
-  
-    useEffect(() => {
-      axios.get(import.meta.env.VITE_BACKEND_URL+ "/api/fruits")
-      .then((response) => {
-        setFruits(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    }, []);
 
   return (
     <>
