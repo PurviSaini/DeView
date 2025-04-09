fa61cf5d67eb02b63dfa713d3e546b97ebe77468
Shikha Dhiman <dshikha4403@gmail.com>
2025-04-09 16:03:09 +0530

    created documentation page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar(){
         <div id="sidebar" className="col p-4">
             <NavLink to="/task" className="text-decoration-none sidebar-link row">Tasks</NavLink>
             <NavLink to="/ideator" className="text-decoration-none sidebar-link row">Ideator</NavLink>
-            <a href='/' className="row">Docs</a>
+            <NavLink to="/documentation" className="text-decoration-none sidebar-link row">Docs</NavLink>
             <a href='/' className="row">Stats</a>
             <a href='/' className="row">Resources</a>
         </div>
