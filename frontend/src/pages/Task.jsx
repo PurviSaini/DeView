cc1b8584af184accd56a9c2cf94cf051664c3886
Purvi Saini <purvitashu450@gmail.com>
2025-04-09 15:32:19 +0530

    correct import

--- frontend/src/pages/Task.jsx
+++ frontend/src/pages/Task.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from 'axios';
 import { Form, Button, Card, Row, Col, Table, Modal } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
