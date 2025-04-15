2c282705f82a418457e86faf0c645f281dc43420
Shikha Dhiman <dshikha4403@gmail.com>
2025-04-15 15:23:06 +0530

    removed comments from documentation page

--- frontend/src/pages/Documentation.jsx
+++ frontend/src/pages/Documentation.jsx
@@ -6,25 +6,9 @@ import ReactMarkdown from 'react-markdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Documentation.css'
 
-const content = "lorem kjd rjgn rjngosjkdfn knf rjfnsldi ekfmwoie eekfwoijt ekekfnj kefo wkfnowienf cjifjoifnkdmf.lorem kjd rjgn rjngosjkdfn knf rjfnsldi ekfmwoie eekfwoijt ekekfnj kefo wkfnowienf cjifjoifnkdmf. lorem kjd rjgn rjngosjkdfn knf rjfnsldi ekfmwoie eekfwoijt ekekfnj kefo wkfnowienf cjifjoifnkdmf. lorem kjd rjgn rjngosjkdfn knf rjfnsldi ekfmwoie eekfwoijt ekekfnj kefo wkfnowienf cjifjoifnkdmf. lorem kjd rjgn rjngosjkdfn knf rjfnsldi ekfmwoie eekfwoijt ekekfnj kefo wkfnowienf cjifjoifnkdmf.lorem kjd rjgn rjngosjkdfn knf rjfnsldi ekfmwoie eekfwoijt ekekfnj kefo wkfnowienf cjifjoifnkdmf. lorem kjd rjgn rjngosjkdfn knf rjfnsldi ekfmwoie eekfwoijt ekekfnj kefo wkfnowienf cjifjoifnkdmf";
 
 export default function Documentation(){
-    // return (
-        // <div>
-        //     <Navbar title="Documentation"/>
-        //     <Sidebar/>
-        //     <Container className="mt-4">
-        //         <Row>
-        //             <Col md={6}>
-        //             <div id='left-container' className="p-4 text-white text-center rounded">{content}</div>
-        //             </Col>
-        //             <Col md={6}>
-        //             <div id='right-container' className="p-4 text-white text-center rounded">{content}</div>
-        //             </Col>
-        //         </Row>
-        //     </Container>
-        // </div>
-    // )
+
     const [markdown, setMarkdown] = useState('# Hello Markdown!');
 
   return (
